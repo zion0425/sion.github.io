d246f5f53e39df7d75d2fbdf3efba846ffe56eef
zion0425 <zion0425@naver.com>
2023-07-11 14:25:43 +0900

    Add ClearStore Method

--- src/main/java/sion/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/sion/hellospring/repository/MemoryMemberRepository.java
@@ -34,4 +34,9 @@ public class MemoryMemberRepository implements  MemberRepository{
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore(){
+        store.clear();
+    }
+
 }
