6009123bf0292e14d39514964ca50d2f688b86e2
zion0425 <zion0425@naver.com>
2023-07-14 17:42:18 +0900

    Add Autowired Annotation to MemberController
    To use Service, Repository in Spring bin

--- src/main/java/sion/hellospring/controller/MemberController.java
+++ src/main/java/sion/hellospring/controller/MemberController.java
@@ -1,2 +1,17 @@
-package sion.hellospring.controller;public class MemberController {
+package sion.hellospring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import sion.hellospring.service.MemberService;
+
+// Spring Container에서 Spring bin이 관리된다.
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    // Spring bin에 등록되어 있는 것을 @Service 객체를 연결한다. == Dependency Injection
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 }
