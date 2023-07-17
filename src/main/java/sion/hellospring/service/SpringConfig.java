90faaa78b6ca31593146f518519851e9d8ddf334
zion0425 <zion0425@naver.com>
2023-07-17 20:35:17 +0900

    Regist Service, Repository in SpringBin

---
+++ src/main/java/sion/hellospring/service/SpringConfig.java
@@ -0,0 +1,19 @@
+package sion.hellospring.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import sion.hellospring.repository.MemberRepository;
+import sion.hellospring.repository.MemoryMemberRepository;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
