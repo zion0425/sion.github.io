46328f5e8ca4027e6bcec7f26085c31285b88850
zion0425 <zion0425@naver.com>
2023-07-31 21:24:36 +0900

    Create JpaMemberRepository

---
+++ src/main/java/sion/hellospring/repository/JpaMemberRepository.java
@@ -0,0 +1,36 @@
+package sion.hellospring.repository;
+
+import jakarta.persistence.EntityManager;
+import sion.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository{
+
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
