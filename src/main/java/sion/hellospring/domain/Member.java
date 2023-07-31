17343c9132edda70952c9c2f9df0351294ade73b
zion0425 <zion0425@naver.com>
2023-07-31 21:22:41 +0900

    Update Identity strategy to Member

--- src/main/java/sion/hellospring/domain/Member.java
+++ src/main/java/sion/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package sion.hellospring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "username")
     private String name;
 
     public String getName() {
