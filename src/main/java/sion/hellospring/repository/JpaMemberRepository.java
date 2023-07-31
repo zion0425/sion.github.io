f04fb2ce3484de4f43983fe75a522993d3fc2676
zion0425 <zion0425@naver.com>
2023-07-31 21:29:19 +0900

    Update JpaMemberRepository

--- src/main/java/sion/hellospring/repository/JpaMemberRepository.java
+++ src/main/java/sion/hellospring/repository/JpaMemberRepository.java
@@ -16,21 +16,26 @@ public class JpaMemberRepository implements MemberRepository{
 
     @Override
     public Member save(Member member) {
+        em.persist(member);
         return null;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return Optional.empty();
+        List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        return em.createQuery("select m from Member m", Member.class) .getResultList();
     }
 }
