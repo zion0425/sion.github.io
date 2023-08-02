f96f54843f799a5b79d7e65210ab4bc67f3cdd67
zion0425 <zion0425@naver.com>
2023-08-02 17:56:36 +0900

    Update Config use Jpa

--- src/main/java/sion/hellospring/service/SpringConfig.java
+++ src/main/java/sion/hellospring/service/SpringConfig.java
@@ -11,22 +11,16 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-//    private DataSource dataSource;
-//
-//    @Autowired
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
     @Bean
@@ -34,6 +28,6 @@ public class SpringConfig {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+//        return new JpaMemberRepository(em);
     }
 }
